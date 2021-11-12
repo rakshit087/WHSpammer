8c37c1fbfbda277636428140435317eaabce21cc
Rakshit Arora <rakshit087@gmail.com>
2021-11-12 20:43:11 +0530

    Fixed scripts.js not getting active tabs

--- public/script.js
+++ public/script.js
@@ -1,17 +1,18 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function sendMessage(message){
-    let params = {
-        currentWindow: true
-    }
-    chrome.tabs.query(params,(tabs)=>{
-        chrome.tabs.sendMessage(tabs[0].id, message)
-    })
+async function sendMessage(message) {
+  let params = {
+    active: true,
+    currentWindow: true,
+  };
+  await chrome.tabs.query(params, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, message);
+  });
 }
 
-document.getElementById("btn").addEventListener("click",async (e) => {
+document.getElementById("btn").addEventListener("click", async (e) => {
   const n_element = document.getElementById("number");
   const message_element = document.getElementById("message");
   const n = n_element.value ? n_element.value : 1;
@@ -21,17 +22,17 @@ document.getElementById("btn").addEventListener("click",async (e) => {
   const form = document.querySelector("form");
   form.classList.add("noDisplay");
   const status = document.getElementById("status");
-  for(let i = n; i>=0; i--) {
-    status.innerHTML = "Sending " +  i + " Messages";
+  for (let i = n; i >= 0; i--) {
+    status.innerHTML = "Sending " + i + " Messages";
     sendMessage(message);
     await sleep(100);
   }
-  status.innerHTML = "Sent <strong>" +  n + "</strong> Messages";
+  status.innerHTML = "Sent <strong>" + n + "</strong> Messages";
   const reset = document.getElementById("reset");
   reset.classList.remove("noDisplay");
-  reset.addEventListener("click",(e)=>{
-      e.srcElement.classList.add("noDisplay");
-      form.classList.remove("noDisplay");
-      status.innerHTML = ""
-  })
+  reset.addEventListener("click", (e) => {
+    e.srcElement.classList.add("noDisplay");
+    form.classList.remove("noDisplay");
+    status.innerHTML = "";
+  });
 });
