bcecd59c8aceb7915b40b8ddd90325b6e481c633
Rakshit Arora <rakshit087@gmail.com>
2021-11-12 21:36:42 +0530

    Added content.js

--- content.js
+++ content.js
@@ -1,4 +1,13 @@
-console.log("Whatsapp Running")
-chrome.runtime.onMessage.addListener((message,sender,sendResponse) => {
-    console.log(message);
-})
+console.log("Whatsapp Running");
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message);
+  var event = new InputEvent("input", {
+    bubbles: true,
+  });
+  var textbox = document.querySelector(
+    "#main > footer > div._2BU3P.tm2tP.copyable-area > div > span:nth-child(2) > div > div._2lMWa > div.p3_M1 > div > div._13NKt.copyable-text.selectable-text"
+  );
+  textbox.textContent = message;
+  textbox.dispatchEvent(event);
+  document.querySelector("button._4sWnG").click();
+});
